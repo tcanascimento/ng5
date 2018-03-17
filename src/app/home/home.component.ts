027e1ee04e9d89af08e1b5950e2aa49f82b6e2ca
Thiago Carreira A. Nascimento <thiago.nascimento@ilegra.com>
2018-03-17 20:22:45 -0300

    adicionado bindings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText: string = "Adiciona um item";
+  goalText: string = "Meu primeiro objetivo";
+
+  goals = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
